2809dc2dbc9f3676a60129d44cd6d743679a2838
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2021-07-27 11:41:47 -0400

    updated api call in separated functions

--- js/index.js
+++ js/index.js
@@ -1,24 +1,54 @@
 (() => {
 	let fetchButton = document.getElementById('fetchButton');
 	let fetchAllButton = document.getElementById('fetchAllButton');
-	// fetchButton.addEventListener('click', fetchButtonClickHandler)
+	fetchButton.addEventListener('click', fetchButtonClickHandler);
 	fetchAllButton.addEventListener('click', fetchAllButtonlickHandler);
 
-	function fetchAllButtonlickHandler() {
-		console.log(' Promise :: fetchAllButtonlickHandler : ');
-		var usersPromise = fetch("https://jsonplaceholder.typicode.com/users").then(res =>
-			res.json()
-		);
+	const fetchPeople = () => {
+		return fetch("https://ghibliapi.herokuapp.com/people")
+			.then(response => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.catch(err => console.error('ERROR :: fetchPeople', err));
+	}
 
-		var albumPromise = fetch("https://jsonplaceholder.typicode.com/albums").then(res =>
-			res.json()
-		);
-		isAllPromiseSettled([usersPromise, albumPromise]).then(results => {
-			console.log(results);
+	function getUsers() {
+		return fetch("https://jsonplaceholder.typicode.com/users")
+			.then(response => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.catch(err => console.error('ERROR :: fetchPeople', err));
+	}
+	function getAlbums() {
+		return fetch("https://jsonplaceholder.typicode.com/albums")
+			.then(response => {
+				if (!response.ok) {
+					throw Error(response.statusText);
+				}
+				return response.json();
+			})
+			.catch(err => console.error('ERROR :: fetchPeople', err));
+	}
+
+	function fetchButtonClickHandler() {
+		getUsers().then(res => {
+			console.log(' fetchButtonClickHandler ::fetchSingleApi : users :', res);
 		});
 	}
+	function fetchAllButtonlickHandler() {
+		isAllPromiseSettled([fetchPeople(), getUsers(), getAlbums()]).then(results => {
+			console.log('Final results : ', results);
+		});
+	}
+
 	function isAllPromiseSettled(promises) {
-		var results = []
+		var results = [];
 		return new Promise(resolve => {
 			promises.forEach((promise, index) => {
 				return Promise.resolve(promise)
